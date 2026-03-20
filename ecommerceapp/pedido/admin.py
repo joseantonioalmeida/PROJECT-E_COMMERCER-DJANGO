f38c427ae1dc810ae5f9b4980beaf3ac9d6995d6
Jose Antonio <joseantonioalmeida217@gmail.com>
2026-03-20 14:54:23 -0300

    Criando os models Pedido e ItemPedido

--- ecommerceapp/pedido/admin.py
+++ ecommerceapp/pedido/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from pedido.models import ItemPedido, Pedido
 
 # Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [ItemPedidoInline]
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    pass
